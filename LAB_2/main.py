1f8d5ada766b1f4dd56006b14baeb04e11dfd7bb
Kirill <gukasyan_kirill@outlook.com>
2019-09-28 15:31:06 +0700

    lab 2 I won't do it again...

--- LAB_2/main.py
+++ LAB_2/main.py
@@ -2,9 +2,9 @@ import translate
 
 
 def main():
-    num = to_hexadecimal(-1.5, 4)
+    num = to_hexadecimal(-123.5, 8)
     print(num)
-    num = from_hexadecimal(num, 4)
+    num = from_hexadecimal(num, 8)
     print(num)
 
 
@@ -12,7 +12,7 @@ def to_hexadecimal(number, byte):
     sign = "0"
     if number < 0:
         sign = "1"
-    number = translate.translate_to(abs(number), 2)
+    number = translate.translate_to(abs(number), 2, byte * 8)
     if number[0] == "0":
         p = -number.find("1") + 1
         number = number[number.find("1")] + "." + number[number.find("1") + 1:]
@@ -25,24 +25,24 @@ def to_hexadecimal(number, byte):
         bit = 11
     else:
         bit = 8
-    order = translate.translate_to(p + 2**(bit - 1) - 1, 2)
+    order = translate.translate_to(p + 2**(bit - 1) - 1, 2, byte * 8)
     order = "0"*(bit - len(order)) + order
-    mantissa = number[1:number.find(".")] + number[number.find(".") + 1:]
+    mantissa = number[1:number.find(".")] + number[number.find(".") + 1:(byte * 8 - bit)]
     mantissa += "0"*((byte * 8 - bit - 1) - len(mantissa))
-    return translate.translate_to_hex(sign + order + mantissa)
+    return translate.translate_to_hex(sign + order + mantissa, byte)
 
 
 def from_hexadecimal(hex_number, byte):
-    binary_number = translate.translate_to(translate.translate_from(hex_number, 16), 2)
+    binary_number = translate.translate_to(translate.translate_from(hex_number, 16, byte * 8), 2, byte * 8)
     if len(binary_number) < byte * 8:
         binary_number = "0"*(byte * 8 - len(binary_number)) + binary_number
     if byte == 8:
         bit = 11
-        p = translate.translate_from(binary_number[1:12], 2) - 1023
+        p = translate.translate_from(binary_number[1:12], 2, byte * 8) - 1023
     elif byte == 4:
         bit = 8
-        p = translate.translate_from(binary_number[1:9], 2) - 127
-    res_number = translate.translate_from("1." + binary_number[bit + 1:],  2) * 2**p
+        p = translate.translate_from(binary_number[1:9], 2, byte * 8) - 127
+    res_number = translate.translate_from("1." + binary_number[bit + 1:],  2, byte * 8) * 2**p
     if binary_number[0] == "0":
         return res_number
     else:
--- LAB_2/translate.py
+++ LAB_2/translate.py
@@ -1,7 +1,7 @@
 import config
 
 
-def translate_to(value, base):
+def translate_to(value, base, count_sign):
     int_part = int(float(value))
     float_part = float(value) - int_part
     res_int_part = ""
@@ -10,7 +10,7 @@ def translate_to(value, base):
         int_part = int_part//base
     res_float_part = ""
     i = 0
-    while float_part != 0 and i < 100:
+    while float_part != 0 and i < count_sign:
         res_float_part += config.enum_lit[(int(float_part * base))]
         float_part = float_part * base - int(float_part * base)
         i += 1
@@ -22,7 +22,7 @@ def translate_to(value, base):
         return res_int_part[::-1]
 
 
-def translate_from(value, base):
+def translate_from(value, base, count_sign):
     value = value.split(".")
     int_part = value[0]
     if len(value) > 1:
@@ -36,14 +36,14 @@ def translate_from(value, base):
         i += 1
     res_float_part = 0
     i = 1
-    for number in float_part:
+    for number in float_part[:count_sign]:
         res_float_part += config.enum[number] * pow(base, -i)
         i += 1
     return float(res_int_part + res_float_part)
 
 
-def translate_to_hex(number):
+def translate_to_hex(number, byte):
     res_number = ""
     for i in range(0, len(number), 4):
-        res_number += config.enum_lit[translate_from(number[i:i+4], 2)]
+        res_number += config.enum_lit[translate_from(number[i:i+4], 2, byte * 8)]
     return res_number
