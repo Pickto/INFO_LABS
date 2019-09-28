fcac5d9454c060f841f04f497336d75d0c4ab8ac
Kirill <gukasyan_kirill@outlook.com>
2019-09-28 14:00:41 +0700

    lab 2 ready

--- LAB_2/main.py
+++ LAB_2/main.py
@@ -2,7 +2,9 @@ import translate
 
 
 def main():
-    num = to_hexadecimal(-1553.734487, 8)
+    num = to_hexadecimal(123.5, 8)
+    print(num)
+    num = from_hexadecimal(num, 8)
     print(num)
 
 
@@ -11,7 +13,11 @@ def to_hexadecimal(number, byte):
     if number < 0:
         sign = "1"
     number = translate.translate_to(abs(number), 2)
-    p = number.find(".") - 1
+    if number.find(".") != -1:
+        p = number.find(".") - 1
+    else:
+        p = len(number) - 1
+        number += ".0"
     if byte == 8:
         bit = 11
     else:
@@ -20,6 +26,21 @@ def to_hexadecimal(number, byte):
     mantissa = number[1:number.find(".")] + number[number.find(".") + 1:]
     mantissa += "0"*(52 - len(mantissa))
     return translate.translate_to_hex(sign + order) + translate.translate_to_hex(mantissa)
+
+
+def from_hexadecimal(hex_number, byte):
+    binary_number = translate.translate_to(translate.translate_from(hex_number, 16), 2)
+    if len(binary_number) < 64:
+        binary_number = "0" + binary_number
+    if byte == 8:
+        p = translate.translate_from(binary_number[1:12], 2) - 1023
+    elif byte == 4:
+        p = translate.translate_from(binary_number[1:12], 2) - 127
+    res_number = translate.translate_from("1." + binary_number[12:],  2) * 2**p
+    if binary_number[0] == "0":
+        return res_number
+    else:
+        return -res_number
 
 
 if __name__ == "__main__":
